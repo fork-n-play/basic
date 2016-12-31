f8981a3d9f069af61d0eb2d4bb6596d24ab97287
Alex Petrosh <petrosh@users.noreply.github.com>
2016-12-31 18:00:31 +0100

    Fix #1

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,8 +1,15 @@
 ---
 ---
+
+// Repository settings
 var	OWNER_NAME = '{{site.github.owner_name}}',
 	PROJECT_NAME = '{{site.github.project_title}}',
 	BUILD_REVISION = '{{ site.github.build_revision }}';
 
+// Polyfills
+{% include_relative polyfills/localStorage.js %}
+{% include_relative polyfills/template.js %}
+
+// fork-n-play scripts
 {% include_relative FnP/loggator.js %}
 {% include_relative FnP/repository.js %}
